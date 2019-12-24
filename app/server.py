369f34a75f95694eade88d500fb21ae56e2679ba
MarkAntipin <antipinsuperstar@yandex.ru>
2019-12-25 00:59:39 +0300

    updte api

--- app/server.py
+++ app/server.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, Response
+
+from app.src.create_image import create_cat_avatar
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!\n'
+@app.route('/<string:user_id>')
+def create_cat_avatar(user_id):
+    image = create_cat_avatar(user_id)
+    return Response(image, mimetype='image/png')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
--- app/src/create_image.py
+++ app/src/create_image.py
@@ -1,21 +1,12 @@
-from time import time
-from pathlib import Path
 from random import seed, choice
 
 from PIL import Image
 
 from app.utils import get_names_from_directory
 from app.settings.paths import IMAGES_DIR
-
-
-EMPTY_PIXEL = (0, 0, 0, 0)
-BLACK_PIXEL = (0, 0, 0, 255)
-WHITE_PIXEL = (255, 255, 255, 255)
-
-IMAGE_MODE = 'RGBA'
-BASE_IMAGE_SIZE = (64, 64)
-
-CONVERTED_SIZE = (640, 640)
+from app.settings.config import (
+    WHITE_PIXEL, BLACK_PIXEL, BASE_IMAGE_SIZE, IMAGE_MODE, CONVERTED_SIZE
+)
 
 
 def make_pixels(image_path):
@@ -37,17 +28,15 @@ def merge_pixels(pixels):
     merged_pixels = []
     for i, pixel_tuple in enumerate(zip(*pixels)):
         if BLACK_PIXEL in pixel_tuple:
-            print(pixel_tuple)
             merged_pixels.append(BLACK_PIXEL)
             continue
         merged_pixels.append(WHITE_PIXEL)
     return merged_pixels
 
 
-if __name__ == '__main__':
-    merged_pixels = merge_pixels(get_all_parts('danilok'))
+def create_cat_avatar(user_id):
+    merged_pixels = merge_pixels(get_all_parts(user_id))
     merged_image = Image.new(IMAGE_MODE, BASE_IMAGE_SIZE)
     merged_image.putdata(merged_pixels)
     resized_image = merged_image.resize(CONVERTED_SIZE)
-    resized_image.show()
-
+    return resized_image
